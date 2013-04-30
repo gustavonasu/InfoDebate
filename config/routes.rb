ea6f1617eb615a2fa5b1f73f6d74f887c09ec582
Gustavo Nasu <gustavonasu@Gustavos-MacBook-Pro.local>
2013-04-30 00:39:19 -0300

    Partial improvements on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,26 @@
 InfoDebate::Application.routes.draw do
 
   namespace :admin do
-    resources :users, :forums, :forum_threads, :comments, :complaints do
+    resources :users, :forums, :forum_threads do
       member do
         get 'change_status/:status_action(/:call_from)', :action => 'change_status', :as => 'change_status'
         get 'show_modal/:call_from', :action => 'show_modal', :as => 'show_modal'
-        get 'answers(/:div_class)', :controller => 'comments', :action => 'answers', :as => 'answers'
+      end
+    end
+    
+    resources :comments do
+      member do
+        get 'change_status/:status_action(/:call_from)', :action => 'change_status', :as => 'change_status'
+        get 'show_modal/:call_from', :action => 'show_modal', :as => 'show_modal'
+        get 'answers(/:div_class)', :action => 'answers', :as => 'answers'
         get 'complaints(/:div_class)', :controller => 'complaints', :action => 'complaints', :as => 'complaints'
+      end
+    end
+    
+    resources :complaints do
+      member do
+        get 'change_status/:status_action(/:call_from)', :action => 'change_status', :as => 'change_status'
+        get 'show_modal/:call_from', :action => 'show_modal', :as => 'show_modal'
       end
     end
   end
