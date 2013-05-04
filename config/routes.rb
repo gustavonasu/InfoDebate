8fc8a8f3300a900f683ea976b683f3e7d345e1bb
Gustavo Nasu <gustavonasu@Gustavos-MacBook-Pro.local>
2013-05-03 23:37:19 -0300

    Removed duplication on the routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,25 @@
 InfoDebate::Application.routes.draw do
 
+  change_status_get = lambda do
+    get 'change_status/:status_action(/:call_from)', :action => 'change_status', :as => 'change_status'
+  end
+
+  show_modal_get = lambda do
+    get 'show_modal/:call_from', :action => 'show_modal', :as => 'show_modal'
+  end
+
   namespace :admin do
     resources :users, :forums, :forum_threads do
       member do
-        get 'change_status/:status_action(/:call_from)', :action => 'change_status', :as => 'change_status'
-        get 'show_modal/:call_from', :action => 'show_modal', :as => 'show_modal'
+        change_status_get.call
+        show_modal_get.call
       end
     end
     
     resources :comments do
       member do
-        get 'change_status/:status_action(/:call_from)', :action => 'change_status', :as => 'change_status'
-        get 'show_modal/:call_from', :action => 'show_modal', :as => 'show_modal'
+        change_status_get.call
+        show_modal_get.call
         get 'answers(/:div_class)', :action => 'answers', :as => 'answers'
         get 'complaints(/:div_class)', :controller => 'complaints', :action => 'complaints', :as => 'complaints'
       end
@@ -19,8 +27,8 @@ InfoDebate::Application.routes.draw do
     
     resources :complaints do
       member do
-        get 'change_status/:status_action(/:call_from)', :action => 'change_status', :as => 'change_status'
-        get 'show_modal/:call_from', :action => 'show_modal', :as => 'show_modal'
+        change_status_get.call
+        show_modal_get.call
       end
     end
   end
