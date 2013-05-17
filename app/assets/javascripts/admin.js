05da6d2a0b00ed758a1cf55d52ea6320ee7cbb70
Gustavo Nasu <gustavonasu@Gustavos-MacBook-Pro.local>
2013-05-16 22:49:16 -0300

    Create a global handler to deal with ajax calls failing

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -12,6 +12,17 @@ $(document).ajaxSuccess(function() {
   active_expander()
 });
 
+$(document).ajaxError(function(event, request, settings) {
+  error_msg = "";
+  if (request.responseText.trim() != "") {
+    error_msg = $.parseJSON(request.responseText).message
+  }
+  if(error_msg == "") error_msg = "Erro na execução da chamada ao servidor"
+  $(".fixed-error-alert > .message").html(error_msg)
+  $(".fixed-error-alert").show()
+  $(".fixed-error-alert").fadeOut(5000);
+});
+
 $(function() {
   active_expander()
 });
